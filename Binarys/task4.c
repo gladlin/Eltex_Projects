6a41a9881bf989dfbc04042c55765b5a0163c672
gladlin <teslyukalina@yandex.ru>
2024-11-18 10:32:31 +0700

    теперь изменяется третий байт в 4-ом задании

--- Binarys/task4.c
+++ Binarys/task4.c
@@ -1,26 +1,23 @@
-#include "binaries.h"
+#include <stdio.h>
 
 void task4() {
-  printf("\nTask 4\nEnter positive number, where is need to change third byte: ");
-  int number = 0;
-  scanf("%d", &number);
-  if (number <= 0) {
-    printf("Please, enter positive integer\n");
-    return;
-  }
+    printf("\nTask 4\nEnter positive number, where is need to change third byte: ");
+    int number = 0;
+    scanf("%d", &number);
+    if (number <= 0) {
+        printf("Please, enter positive integer\n");
+        return;
+    }
 
-  printf("\nPlease enter digit, what should be insert into third byte: ");
-  int byte = -1;
-  scanf("%d", &byte);
-  if (byte < 0) {
-    printf("Please, enter positive byte\n");
-    return;
-  }
+    printf("\nPlease enter byte (0-255), which should be inserted into third byte: ");
+    int byte = 0;
+    scanf("%d", &byte);
+    if (byte > 255 || byte < 0) {
+        printf("Please, enter a valid byte (0-255)\n");
+        return;
+    }
+    number &= (0xFF << 16);
+    number |= (byte << 16);
 
-  if (byte == 1)
-    number |= (byte << 2);
-  else if (byte == 0)
-    number &= ~(1 << 2);
-
-  printf("Result: %d\n", number);
+    printf("Result: %u\n", number);
 }
---
+++ Pointers/task1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void enterThird(char *number, int third_byte)
+{
+    number += 2;
+    *number = third_byte;
+}
+
+void printBytes(char *number)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        printf("%d\t", *number);
+        number++;
+    }
+}
+
+int main()
+{
+    int num = -1;
+    char c;
+    printf("Enter positive integer: ");
+    if(scanf("%d%c", &num, &c) != 2 ||  c == '.' || num < 0)
+        return -1;
+    printf("Enter number, that must be entered in third byte: ");
+    int third_byte = -1;
+    if(scanf("%d%c", &third_byte, &c) != 2 ||  c == '.' || third_byte < 0)
+        return -1;
+    
+    printBytes((char *)&num);
+    enterThird((char *)&num, third_byte);
+    printf("\nResult: %d\n", num);
+printBytes((char *)&num);
+    return 0;
+}
